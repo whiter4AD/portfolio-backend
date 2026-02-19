ecdcc7df6ef0ba1d48097e50c4846822014ddfc9
whiter4AD <hrustimposter@gmail.com>
2026-02-19 17:53:42 +0500

    force python 3.11

--- app/core/config.py
+++ app/core/config.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
-        env_file_encoding = "utf-8"
+        env_file_encoding = "utf-8"git add .
 
 
 settings = Settings()
